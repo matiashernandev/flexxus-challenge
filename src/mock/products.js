7faec2f003212c59c33d98eddc86d8f587e4f3b4
nkwaaaa <nkwaaaa@gmail.com>
2023-08-14 08:18:18 -0300

    add filter with code and clean store

---
+++ src/mock/products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: "HEADSET GAMER HAVIT HV-H2232 D",
+    code: "CODE143706",
+    price: "$3000",
+    img: "/assets/img/headset01.webp",
+  },
+  {
+    id: 3,
+    name: "HEADSET GAMER NOGA STORMER",
+    code: "CODE185756",
+    price: "$2550",
+    img: "/assets/img/headset02.webp",
+  },
+  {
+    id: 2,
+    name: "HEADSET GAMER CONSTRICTOR",
+    code: "CODE143456",
+    price: "$2700",
+    img: "/assets/img/headset03.webp",
+  },
+  {
+    id: 4,
+    name: "MOUSE GAMER STORMER",
+    code: "CODE895432",
+    price: "$1500",
+    img: "/assets/img/mouse01.webp",
+  },
+]
--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,5 @@
 import { create } from "zustand"
-
-const products = [
-  {
-    id: 1,
-    name: "HEADSET GAMER HAVIT HV-H2232 D",
-    code: "CODE143706",
-    price: 3000,
-    img: "src/assets/img/headset01.png",
-  },
-  {
-    id: 3,
-    name: "HEADSET GAMER NOGA STORMER",
-    code: "CODE185756",
-    price: 2550,
-    img: "src/assets/img/headset02.png",
-  },
-  {
-    id: 2,
-    name: "HEADSET GAMER CONSTRICTOR",
-    code: "CODE143456",
-    price: 2700,
-    img: "src/assets/img/headset03.png",
-  },
-  {
-    id: 4,
-    name: "MOUSE GAMER STORMER",
-    code: "CODE895432",
-    price: 1500,
-    img: "src/assets/img/mouse01.png",
-  },
-]
+import { products } from "../mock/products"
 
 const useStore = create((set) => ({
   searchKeyword: "",
@@ -38,8 +8,10 @@ const useStore = create((set) => ({
   setSearchKeyword: (keyword) =>
     set((state) => ({
       searchKeyword: keyword,
-      filteredProducts: state.products.filter((product) =>
-        product.name.toLowerCase().includes(keyword.toLowerCase())
+      filteredProducts: state.products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(keyword.toLowerCase()) ||
+          product.code.toLowerCase().includes(keyword.toLowerCase())
       ),
     })),
 }))
